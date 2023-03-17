faffb6bf00abcf7f7d7c1e822e0af24ab797001b
Virul Nirmala <virulnirmala24@gmail.com>
2023-03-17 17:59:37 +0530

    msg sending part done

--- pages/Msg.tsx
+++ pages/Msg.tsx
@@ -20,7 +20,7 @@ export default function Msg({route, navigation}) {
   const windowWidth = Dimensions.get('window').width;
   const maxMsgWidth = 0.7 * windowWidth;
   const [data, setData] = useState([]);
-  const [message, setmessage] = useState('heloo mello mass melao');
+  const [message, setmessage] = useState('');
   const Left = <Icon name="left" size={20} style={{color: '#0008'}} />;
   const Check = (
     <Icon2
@@ -92,9 +92,28 @@ export default function Msg({route, navigation}) {
             placeholder="Type your message here"
             style={style.textInput}
             multiline={true}
+            onChangeText={setmessage}
+            value={message}
           />
         </View>
-        <TouchableOpacity style={style.sendButton}>
+        <TouchableOpacity style={style.sendButton} onPress={async ()=>{
+            const userJSON = await AsyncStorage.getItem('user');
+            var userJS = JSON.parse(userJSON);
+            var form = new FormData();
+            form.append('from_user_id', userJS.id);
+            form.append('to_user_id', route.params.id);
+            form.append("msg", message);
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = ()=>{
+                if(xhttp.readyState == 4 && xhttp.status == 200){
+                    // Alert.alert("Response",xhttp.responseText);
+                    setmessage('');
+                }
+            }
+            xhttp.open("POST","http://10.0.2.2/viva_react_cht/saveChat.php");
+            xhttp.send(form);
+        }}>
           <Icon2 name="ios-send" size={15} style={{color: '#0008'}} />
         </TouchableOpacity>
       </View>
